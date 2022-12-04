541aeecfc9c520479efea28bc01a1c617083ebb1
Shreeya Adhikari <shreeya.4708@kistcollege.edu.np>
2022-12-04 22:44:41 +0545

    Image CRUD

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Image;
 use Illuminate\Http\Request;
 
 class ImageController extends Controller
 {
     //
+
+    public  function index()
+    {
+        $images = Image::all();
+        // return redirect()->route('welcome');
+        return view('welcome', compact('images'));
+
+    }
     // file()-> handles filesize
     public function uploadImage(Request $req)
     {
@@ -23,9 +32,25 @@ class ImageController extends Controller
         // return back();
 
         if ($req->hasFile('image')) {
-            $image = $req->file('image');
-        //    $response = $image->store('images', 'public');
-            $file_path = $image->store('images', 'public');
+            $image = new Image();
+            // $image = $req->file('image');
+            //    $response = $image->store('images', 'public');
+            // $file_path = $image->store('images', 'public');
+
+
+            $name = $req->file('image');
+            $file_path = $name->store('images', 'public');
+            // $book->file=$request->file('file')->store('apiDocs','public');
+            
+
+            $image->name = $file_path;
+            // $image1->name = $req->file('image')->store('images', 'public');
+
+            // $name = $req->file('image')->getClientOriginalName();
+            // $req->file('image')->storeAs('public/iamges/',$name);
+            // $image1->name = $name;
+        
+            $image->save();
             return back();
         }
 
--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/',[ImageController::class, 'index'])->name('welcome');
 Route::post('/upload-image',[ImageController::class, 'uploadImage'])->name('uploadImage');
